de8cf29f062335acc73d4caeb6f70eed3edfd402
Tobias <pito.mailing@gmail.com>
2023-03-23 20:54:46 +0100

    Add kicked event, start new json data gen notebook

--- utils/.ipynb_checkpoints/parser-checkpoint.py
+++ utils/.ipynb_checkpoints/parser-checkpoint.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+from tqdm.auto import tqdm
 
 PATTERNS = {
     'join_hate_kick_regex':re.compile('J.*R.*L')
@@ -21,6 +22,8 @@ def parse_event(alliance_messages, alliance_actions, account_id):
         alliance_actions.loc[(alliance_actions.account_id == account_id) & (alliance_actions.action=='join'),'event'] = 'J'
         #user leave event
         alliance_actions.loc[(alliance_actions.account_id == account_id) & (alliance_actions.action=='leave'),'event'] = 'L'
+        #user got kicked event
+        alliance_actions.loc[(alliance_actions.account_id == account_id) & (alliance_actions.action=='kicked_out'),'event'] = 'K'
         
         alliance_actions = pd.DataFrame({'type':1, 'id':alliance_actions.index,  'timestamp':alliance_actions.timestamp, 'event':alliance_actions.event})
     
@@ -46,7 +49,6 @@ def find_all_patterns(data):
         for account_id in account_ids:
             finds = parse_event(alliance_messages=alliance_messages, alliance_actions=alliance_actions, account_id=account_id)
             if len(finds):
-                print(finds)
                 found_dict[account_id] = finds
         if len(found_dict):
             results[alliance_id]=found_dict

1fc560f26b0361dbf7d3f0bf510788bfd6638334
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2023-03-24 02:25:47 +0100

    multiple colors for different patterns

--- website/src/components/ViolationCard.vue
+++ website/src/components/ViolationCard.vue
@@ -6,7 +6,7 @@ import QuickActions from './QuickActions.vue'
 import { Patterns, Pattern } from '../ts/Pattern';
 
 
-defineProps<{
+const props = defineProps<{
     violation: Violation,
 }>()
 
@@ -26,6 +26,16 @@ function toggleAlliance(): void {
 function violationNames(pattern: Patterns): string[] {
     return Object.entries(pattern).map((entry: [string, Pattern]) => Object.entries(Object.entries(entry[1])[0])[0][1] as unknown as string)
 }
+
+const names = [...new Set(violationNames(props.violation.theViolations))]
+const textColors = ['text-red-400', 'text-yellow-400', 'text-blue-400']
+const mainColors = ['bg-red-100', 'bg-yellow-100', 'bg-blue-100']
+const borderColors= ['border-red-800', 'border-yellow-800','border-blue-800']
+
+function nameToColors(name: string) {
+    const index = names.indexOf(name)% names.length
+    return textColors[index ] + ' ' + mainColors[index ] + ' ' + borderColors[index ]
+}
 </script>
 
 <template>
@@ -38,7 +48,7 @@ function violationNames(pattern: Patterns): string[] {
         <div class="flex flex-row mb-2">   
             <div 
         v-for="name in violationNames(violation.theViolations)" :key="name"
-        class="flex w-60 shrink-0 p-2 m-2 text-sm text-red-400 border border-red-800 rounded-lg bg-red-100" role="alert">
+        class="flex w-60 shrink-0 p-2 m-2 text-sm border rounded-lg" :class="nameToColors(name)" role="alert">
             <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Info</span>
             <div>
